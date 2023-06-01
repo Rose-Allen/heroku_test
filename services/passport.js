39e9116653ac61a6c941dce754b5588b135bf7b4
Dimash <d.esengeldiev@mail.ru>
2023-06-01 15:24:51 +0600

    update

--- services/passport.js
+++ services/passport.js
@@ -18,6 +18,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     function (accessToken, refreshToken, profile, cb) {
       console.log("access token", accessToken);
